5f6c8327f1a518b708c8d24574b644c68a7274b9
Pedro Minicz <pedrominicz@gmail.com>
2020-09-28 20:57:48 -0300

    Use De Bruijn indices for bound variables

--- Syntax.hs
+++ Syntax.hs
@@ -1,19 +1,33 @@
 module Syntax where
 
+import Data.List
+
 type Name = String
 
 data Expr
-  = Var Name
-  | Lam Name Expr
-  | App Expr Expr
+  = App Expr Expr
+  | Bound Int
+  | Free Name
+  | Lam Expr
   deriving (Eq)
 
-instance Show Expr where
-  show = go
-    where
-    go (Var v) = v
-    go (Lam v b) = "\\" ++ v ++ "." ++ go b
-    go (App f a) = go f ++ parens a
+free :: Expr -> [Name]
+free (App f a) = free f ++ free a
+free (Bound x) = []
+free (Free x)  = [x]
+free (Lam b)   = free b
 
-    parens (Var v) = v
-    parens x = "(" ++ go x ++ ")"
+instance Show Expr where
+  show expr = go 0 expr
+    where
+    go n (App f a) = go n f ++ parens n a
+    go n (Bound x) = names !! (n - x - 1)
+    go n (Free x)  = x
+    go n (Lam b)   = "\\" ++ names !! n ++ "." ++ go (n + 1) b
+
+    parens n (Bound x) = go n (Bound x)
+    parens n (Free x)  = go n (Free x)
+    parens n x         = "(" ++ go n x ++ ")"
+
+    -- This explodes if indexed beyond 25 or less.
+    names = map return ['a'..'z'] \\ free expr
