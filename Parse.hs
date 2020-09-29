6f3769f6f5fe4f5c9155f06f35d9b749b4cd6c61
Pedro Minicz <pedrominicz@gmail.com>
2020-09-28 23:36:26 -0300

    Rewrite parser.
    Rewrite the parser due to not begin able to change reduction that is a
    order in Happy. Upon encountering a lambda token (`\`) a monadic action
    adding the bound variables to the local context should be executed.
    This is not possible in Happy since it uses depth-first reduction, i.e.
    the body is already reduced by the time bound variables are.

---
+++ Parse.hs
@@ -0,0 +1,49 @@
+module Parse where
+
+import Expr
+
+import Control.Monad.Reader
+import Data.List
+import Text.Parsec hiding (parse)
+
+type Parser = ParsecT String () (Reader [Name])
+
+parse :: String -> Maybe Expr
+parse s =
+  case runReader (runParserT (whitespace *> expression <* eof) () "" s) [] of
+    Left e  -> error $ show e
+    Right e -> Just e
+
+expression :: Parser Expr
+expression = lambda <|> application
+
+lambda :: Parser Expr
+lambda = do
+  try $ char '\\' *> whitespace
+  xs <- many1 name
+  char '.' *> whitespace
+  b <- local (reverse xs ++) expression
+  return $ foldr (const Lam) b xs
+
+application :: Parser Expr
+application = (parens expression <|> variable) `chainl1` return App
+
+variable :: Parser Expr
+variable = do
+  env <- ask
+  x <- name
+  case elemIndex x env of
+    Just x  -> return $ Bound x
+    Nothing -> return $ Free x
+
+name :: Parser Name
+name = satisfy (`elem` ['a'..'z']) <* whitespace
+
+parens :: Parser a -> Parser a
+parens p = between open close p
+  where
+  open = char '(' <* whitespace
+  close = char ')' <* whitespace
+
+whitespace :: Parser ()
+whitespace = skipMany space
