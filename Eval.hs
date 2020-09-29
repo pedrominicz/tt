409def605c2404512debbf6fc830cd12a8998f85
Pedro Minicz <pedrominicz@gmail.com>
2020-09-29 10:15:28 -0300

    Update eval to use De Bruijn indices

--- Eval.hs
+++ Eval.hs
@@ -1,17 +1,17 @@
 module Eval where
 
-import Syntax
+import Expr
 
 eval :: Expr -> Expr
-eval (Lam v b) = Lam v (eval b)
+eval (Lam b) = Lam (eval b)
 eval (App f a) =
   case eval f of
-    Lam v b -> eval $ subst v a b
+    Lam b -> eval $ subst 0 a b
     f -> App f (eval a)
 eval x = x
 
-subst :: Name -> Expr -> Expr -> Expr
-subst v x (Var v')   | v == v' = x
-subst v x (Lam v' b) | v /= v' = Lam v' (subst v x b)
-subst v x (App f a)            = App (subst v x f) (subst v x a)
-subst _ _ x                    = x
+subst :: Int -> Expr -> Expr -> Expr
+subst i x (App f a) = App (subst i x f) (subst i x a)
+subst i x (Bound i') | i == i' = x
+subst i x (Lam b) = Lam (subst (i + 1) x b)
+subst _ _ x = x
