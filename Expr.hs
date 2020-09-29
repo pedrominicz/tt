90d3fb17a7bd951c64755f8e39f07ae868b92605
Pedro Minicz <pedrominicz@gmail.com>
2020-09-29 10:12:23 -0300

    Chore: change indentation

--- Expr.hs
+++ Expr.hs
@@ -14,23 +14,23 @@ data Expr
 free :: Expr -> [Name]
 free (App f a) = free f ++ free a
 free (Bound i) = []
-free (Free x)  = [x]
-free (Lam b)   = free b
+free (Free x) = [x]
+free (Lam b) = free b
 
 instance Show Expr where
   show expr = go 0 expr
     where
     go n (App f a) = parens n f ++ parens' n a
     go n (Bound i) = [names !! (n - i - 1)]
-    go n (Free x)  = [x]
-    go n (Lam b)   = "\\" ++ [names !! n] ++ "." ++ go (n + 1) b
+    go n (Free x) = [x]
+    go n (Lam b) = "\\" ++ [names !! n] ++ "." ++ go (n + 1) b
 
     parens n (Lam b) = "(" ++ go n (Lam b) ++ ")"
-    parens n x       = go n x
+    parens n x = go n x
 
     parens' n (Bound i) = go n (Bound i)
-    parens' n (Free x)  = go n (Free x)
-    parens' n x         = "(" ++ go n x ++ ")"
+    parens' n (Free x) = go n (Free x)
+    parens' n x = "(" ++ go n x ++ ")"
 
     -- This explodes if indexed beyond 25 or less.
     names = ['a'..'z'] \\ free expr
