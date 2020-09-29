06464098efd7819fd571e765eb609e378cafbae9
Pedro Minicz <pedrominicz@gmail.com>
2020-09-28 23:31:19 -0300

    Fix lambdas not parenthesizing correctly

--- Expr.hs
+++ Expr.hs
@@ -20,14 +20,17 @@ free (Lam b)   = free b
 instance Show Expr where
   show expr = go 0 expr
     where
-    go n (App f a) = go n f ++ parens n a
+    go n (App f a) = parens n f ++ parens' n a
     go n (Bound x) = [names !! (n - x - 1)]
     go n (Free x)  = [x]
     go n (Lam b)   = "\\" ++ [names !! n] ++ "." ++ go (n + 1) b
 
-    parens n (Bound x) = go n (Bound x)
-    parens n (Free x)  = go n (Free x)
-    parens n x         = "(" ++ go n x ++ ")"
+    parens n (Lam b) = "(" ++ go n (Lam b) ++ ")"
+    parens n x       = go n x
+
+    parens' n (Bound x) = go n (Bound x)
+    parens' n (Free x)  = go n (Free x)
+    parens' n x         = "(" ++ go n x ++ ")"
 
     -- This explodes if indexed beyond 25 or less.
     names = ['a'..'z'] \\ free expr
--- Test.hs
+++ Test.hs
@@ -12,6 +12,7 @@ test1 = "\\a.\\b.a" == show (Lam (Lam (Bound 1)))
 test2 = "\\b.\\c.ab" == show (Lam (Lam (App (Free 'a') (Bound 1))))
 test3 = "\\a.\\b.\\c.ac(bc)" == show (Lam (Lam (Lam (App (App (Bound 2) (Bound 0)) (App (Bound 1) (Bound 0))))))
 test4 = "\\b.\\d.\\e.cbe(d(ae))" == show (Lam (Lam (Lam (App (App (App (Free 'c') (Bound 2)) (Bound 0)) (App (Bound 1) (App (Free 'a') (Bound 0)))))))
+test5 = "(\\a.a)(\\a.a)" == show (App (Lam (Bound 0)) (Lam (Bound 0)))
 
 tests :: [Bool]
 tests = [test0, test1, test2, test3, test4]
